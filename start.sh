4e5d4cf5d45501c22787dc7ec381fb25cf1df3d1
aijl2008 <Jerry790928>
2018-03-08 19:01:19 +0800

    up

--- start.sh
+++ start.sh
@@ -1,28 +1,22 @@
 #!/bin/sh
-pwd=`pwd`/src
-oldpwd=`pwd`/src
+runtime_path=/data/docker/container/default
+original_path=`pwd`/src
 tag="laravel"
-ip="0.0.0.0"
+ip="127.0.0.1"
 if [ $1 ];then
     ip=$1
 fi
 if [ $2 ];then
-    pwd=$2
-    if [ ! -d pwd ];
-    then
-        mkdir -p ${pwd}
-    fi
-    echo ${oldpwd}
-    find ${oldpwd} -type f
-
-    cp -r ${oldpwd} ${pwd}
-    echo ${pwd}
-    find ${pwd} -type f
+    runtime_path=$2
 fi
 if [ $3 ];then
     tag=$3
 fi
-echo "run -d \
+if [ ! -d runtime_path ];then
+ mkdir -p ${runtime_path}
+fi
+cp -r ${original_path} ${runtime_path}
+echo "docker run -d \
 --name ${tag} \
 -p ${ip}:80:80 \
 -p ${ip}:443:443 \
@@ -30,25 +24,57 @@ echo "run -d \
 -p ${ip}:9999:9999 \
 -p ${ip}:27017:27017 \
 -p ${ip}:4501:22 \
--v ${pwd}/log/mongodb:/var/log/mongodb \
--v ${pwd}/log/mysqld.log:/var/log/mysqld.log \
--v ${pwd}/log/nginx:/var/log/nginx \
--v ${pwd}/log/php-fpm:/var/log/php-fpm \
--v ${pwd}/log/php:/var/log/php \
--v ${pwd}/log/redis:/var/log/redis \
--v ${pwd}/log/supervisor:/var/log/supervisor \
--v ${pwd}/etc/nginx:/etc/nginx \
--v ${pwd}/etc/php-fpm.conf:/etc/php-fpm.conf \
--v ${pwd}/etc/php-fpm.d:/etc/php-fpm.d \
--v ${pwd}/etc/php.ini:/etc/php.ini \
--v ${pwd}/etc/php.d:/etc/php.d \
--v ${pwd}/etc/redis.conf:/etc/redis.conf \
--v ${pwd}/etc/supervisord.conf:/etc/supervisord.conf \
--v ${pwd}/etc/supervisord.d:/etc/supervisord.d \
--v ${pwd}/etc/my.cnf:/etc/my.cnf \
--v ${pwd}/etc/mongod.conf:/etc/mongod.cnf \
--v ${pwd}/var/lib/mongodb:/var/lib/mongodb \
--v ${pwd}/var/lib/mysql:/var/lib/mysql \
--v ${pwd}/var/lib/redis:/var/lib/redis \
--v ${pwd}/webroot:/data/webroot \
+-v ${runtime_path}/log/mongodb:/var/log/mongodb \
+-v ${runtime_path}/log/mysqld.log:/var/log/mysqld.log \
+-v ${runtime_path}/log/nginx:/var/log/nginx \
+-v ${runtime_path}/log/php-fpm:/var/log/php-fpm \
+-v ${runtime_path}/log/php:/var/log/php \
+-v ${runtime_path}/log/redis:/var/log/redis \
+-v ${runtime_path}/log/supervisor:/var/log/supervisor \
+-v ${runtime_path}/etc/nginx:/etc/nginx \
+-v ${runtime_path}/etc/php-fpm.conf:/etc/php-fpm.conf \
+-v ${runtime_path}/etc/php-fpm.d:/etc/php-fpm.d \
+-v ${runtime_path}/etc/php.ini:/etc/php.ini \
+-v ${runtime_path}/etc/php.d:/etc/php.d \
+-v ${runtime_path}/etc/redis.conf:/etc/redis.conf \
+-v ${runtime_path}/etc/supervisord.conf:/etc/supervisord.conf \
+-v ${runtime_path}/etc/supervisord.d:/etc/supervisord.d \
+-v ${runtime_path}/etc/my.cnf:/etc/my.cnf \
+-v ${runtime_path}/etc/mongod.conf:/etc/mongod.cnf \
+-v ${runtime_path}/var/lib/mongodb:/var/lib/mongodb \
+-v ${runtime_path}/var/lib/mysql:/var/lib/mysql \
+-v ${runtime_path}/var/lib/redis:/var/lib/redis \
+-v ${runtime_path}/webroot:/data/webroot \
 artron/laravel-docker:1.0"
+docker run -d \
+--name ${tag} \
+-p ${ip}:80:80 \
+-p ${ip}:443:443 \
+-p ${ip}:3306:3308 \
+-p ${ip}:9999:9999 \
+-p ${ip}:27017:27017 \
+-p ${ip}:4501:22 \
+-v ${runtime_path}/log/mongodb:/var/log/mongodb \
+-v ${runtime_path}/log/mysqld.log:/var/log/mysqld.log \
+-v ${runtime_path}/log/nginx:/var/log/nginx \
+-v ${runtime_path}/log/php-fpm:/var/log/php-fpm \
+-v ${runtime_path}/log/php:/var/log/php \
+-v ${runtime_path}/log/redis:/var/log/redis \
+-v ${runtime_path}/log/supervisor:/var/log/supervisor \
+-v ${runtime_path}/etc/nginx:/etc/nginx \
+-v ${runtime_path}/etc/php-fpm.conf:/etc/php-fpm.conf \
+-v ${runtime_path}/etc/php-fpm.d:/etc/php-fpm.d \
+-v ${runtime_path}/etc/php.ini:/etc/php.ini \
+-v ${runtime_path}/etc/php.d:/etc/php.d \
+-v ${runtime_path}/etc/redis.conf:/etc/redis.conf \
+-v ${runtime_path}/etc/supervisord.conf:/etc/supervisord.conf \
+-v ${runtime_path}/etc/supervisord.d:/etc/supervisord.d \
+-v ${runtime_path}/etc/my.cnf:/etc/my.cnf \
+-v ${runtime_path}/etc/mongod.conf:/etc/mongod.cnf \
+-v ${runtime_path}/var/lib/mongodb:/var/lib/mongodb \
+-v ${runtime_path}/var/lib/mysql:/var/lib/mysql \
+-v ${runtime_path}/var/lib/redis:/var/lib/redis \
+-v ${runtime_path}/webroot:/data/webroot \
+artron/laravel-docker:1.0
+
+echo "docker exec -it ${tag} /bin/bash"
